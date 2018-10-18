b253ac1a03797c6600a04b80eb442976db43a951
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-18 12:58:33 +0200

    Install more packages

--- scripts/install_razzo.sh
+++ scripts/install_razzo.sh
@@ -12,5 +12,13 @@ module load GCCcore/4.9.3
 module load XZ/5.2.2-foss-2016a
 #module load R/3.3.1-foss-2016a
 module load R
-Rscript -e 'devtools::install_github("richelbilderbeek/razzo", ref = "devel")'
+Rscript -e 'devtools::install_github("richelbilderbeek/beautier", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/beastier", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/babette", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/tracerer", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/mauricer", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/pirouette", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/becosys", ref = "develop")'
+Rscript -e 'devtools::install_github("richelbilderbeek/razzo", ref = "develop")'
+Rscript -e 'devtools::install_github("Giappo/mbd", ref = "develop")'
 Rscript -e 'beastier::install_beast2()'
