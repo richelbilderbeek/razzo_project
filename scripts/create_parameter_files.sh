0beac9a2b3744059c5a23d6517ffb04b596b8073
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-22 12:57:01 +0200

    Update script name [skip ci]

--- scripts/create_parameter_files.sh
+++ scripts/create_parameter_files.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=1G
-#SBATCH --job-name=create_input_files_general
-#SBATCH --output=create_input_files_general.log
+#SBATCH --job-name=raz_create_parameters_files
+#SBATCH --output=raz_create_parameters_files.log
 module load GCCcore/4.9.3 
 module load XZ/5.2.2-foss-2016a
 module load R
