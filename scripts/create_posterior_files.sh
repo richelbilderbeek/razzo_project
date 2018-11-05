888e8d9b6d09320ea11173304b3cb517fab6cf42
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-05 13:48:45 +0100

    Create posterior files

---
+++ scripts/create_posterior_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Create input files
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=raz_create_posterior_files
+#SBATCH --output=raz_create_create_posterior_files.log
+module load GCCcore/4.9.3 
+module load XZ/5.2.2-foss-2016a
+module load R
+for filename in $(find . | egrep "parameters\.csv")
+do
+  echo $filename
+  Rscript -e 'razzo::raz_create_mbd_posterior_files("'$filename'")'
+  Rscript -e 'razzo::raz_create_bd_posterior_files("'$filename'")'
+done
