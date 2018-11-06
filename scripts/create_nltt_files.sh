727b630e7c61a3425a1a64fd018b0cc946cc0e3f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-06 13:13:56 +0100

    Create nLTT files

---
+++ scripts/create_nltt_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Create nLTT files
+# Peregrine directives:
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=raz_create_nltt_files
+#SBATCH --output=raz_create_create_nltt_files.log
+module load GCCcore/4.9.3 
+module load XZ/5.2.2-foss-2016a
+module load R
+for filename in $(find . | egrep "parameters\.csv")
+do
+  echo $filename
+  echo "MBD"
+  Rscript -e 'razzo::raz_create_mbd_nltt_files("'$filename'")'
+  echo "BD"
+  Rscript -e 'razzo::raz_create_bd_nltt_files("'$filename'")'
+done
