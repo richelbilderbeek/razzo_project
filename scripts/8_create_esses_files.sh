3ccb36f595a439b1565b3abf9f6138769c8c4289
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-21 09:47:59 +0100

    Assume 'razzo' is script-friendly

--- scripts/8_create_esses_files.sh
+++ scripts/8_create_esses_files.sh
@@ -11,4 +11,4 @@
 module load GCCcore/4.9.3 
 module load XZ/5.2.2-foss-2016a
 module load R
-Rscript -e 'razzo::raz_create_esses_file("scripts/razzo_project")'
+Rscript -e 'razzo::raz_create_esses_file()'
