f8ae913d76487edb29caae22a140d0019a9050dc
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-06 11:15:26 +0100

    Prepare to measure marginal likelihood estimations [skip ci]

---
+++ scripts/measure_est_marg_liks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Measure how long it takes to estimate marginal likelihoods
+# by doing so on each FASTA file in the folder
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=measure_est_marg_liks
+#SBATCH --output=measure_est_marg_liks.log
+module load GCCcore/4.9.3 
+module load XZ/5.2.2-foss-2016a
+module load R
+
+for filename in $(find . | egrep "\.fas")
+do
+  echo $filename
+  sbatch est_marg_lik.sh $filename
+done
