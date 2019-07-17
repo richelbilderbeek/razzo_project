204f4cb7ad61eb4b8fcd005c3624626a781d2930
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-17 15:33:46 +0200

    Use new regex, progress https://github.com/richelbilderbeek/razzo/issues/224

--- scripts/rerun_failed.sh
+++ scripts/rerun_failed.sh
@@ -24,7 +24,9 @@ module load MPFR
 echo "Host name: "$HOSTNAME
 
 # Collect the filenames
-filenames=$(cat $(egrep -Rl FAILED | egrep "^run_") | egrep "parameters\.RDa" | egrep -o "\".*\"" | sed -e "s|.\/|$PWD\/|" | sed -e "s|\"||g")
+
+
+filenames=$(cat $(egrep -Rl FAILED | egrep "^run_") | egrep "parameters\.RDa" | egrep -o "\".*\"" | sed -e "s|\"||g" | egrep -v "^\./data" | sort | uniq)
 
 for filename in $filenames
 do
