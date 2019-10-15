a22645f9149830ae51ff4a70f1f8a54788489640
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-15 16:53:13 +0200

    Regenerate data with testing parameter set

--- scripts/regen_data.sh
+++ scripts/regen_data.sh
@@ -39,7 +39,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 time ./scripts/1_install_razzo.sh
-time ./scripts/2_create_parameter_files.sh
+time ./scripts/2_create_parameter_files.sh test
 time ./scripts/3_run_razzo.sh test
 time ./scripts/7_create_nltt_stats_file.sh
 time ./scripts/8_create_esses_files.sh
