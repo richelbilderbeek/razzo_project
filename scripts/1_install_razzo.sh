81f341a22a3ec2eab73422efbd1242aa5f5c9452
richelbilderbeek <richel@richelbilderbeek.nl>
2019-12-17 07:20:30 +0100

    Update packages

--- scripts/1_install_razzo.sh
+++ scripts/1_install_razzo.sh
@@ -20,6 +20,14 @@
 #SBATCH --job-name=1_install_razzo
 #SBATCH --output=1_install_razzo.log
 module load R
+
+# From https://www.r-bloggers.com/update-all-user-installed-r-packages-again/
+install.packages( 
+  lib  = lib <- .libPaths()[1],
+  pkgs = as.data.frame(installed.packages(lib), stringsAsFactors=FALSE)$Package,
+  type = 'source'
+)
+
 Rscript -e 'devtools::install_github("richelbilderbeek/nLTT")'
 Rscript -e 'devtools::install_github("ropensci/beautier")'
 Rscript -e 'devtools::install_github("ropensci/tracerer", ref = "develop")'
@@ -34,7 +42,8 @@ Rscript -e 'devtools::install_github("Giappo/mbd.SimTrees")'
 Rscript -e 'devtools::install_github("richelbilderbeek/becosys")'
 Rscript -e 'devtools::install_github("richelbilderbeek/peregrine")'
 Rscript -e 'devtools::install_github("richelbilderbeek/razzo")'
-Rscript -e 'if (beastier::is_beast2_installed()) beastier::uninstall_beast2()'
 Rscript -e 'if (mauricer::is_beast2_pkg_installed("NS")) mauricer::uninstall_beast2_pkg("NS")'
+Rscript -e 'if (beastier::is_beast2_installed()) beastier::uninstall_beast2()'
 Rscript -e 'if (!beastier::is_beast2_installed()) beastier::install_beast2()'
 Rscript -e 'if (!mauricer::is_beast2_pkg_installed("NS")) mauricer::install_beast2_pkg("NS")'
+
