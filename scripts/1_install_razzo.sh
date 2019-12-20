bcede936b0938cbff2d0ff566224da751b409996
richelbilderbeek <richel@richelbilderbeek.nl>
2019-12-20 11:17:07 +0100

    Load modules line by line

--- scripts/1_install_razzo.sh
+++ scripts/1_install_razzo.sh
@@ -19,7 +19,8 @@
 #SBATCH --mem=1G
 #SBATCH --job-name=1_install_razzo
 #SBATCH --output=1_install_razzo.log
-module load R HDF5/1.10.1-foss-2018a
+module load R
+module load HDF5/1.10.1-foss-2018a
 Rscript -e 'install.packages("BiocManager")'
 Rscript -e 'BiocManager::install(version = "3.10")'
 Rscript -e 'BiocManager::install()'
